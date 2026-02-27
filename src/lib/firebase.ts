952f7c4d3b2e754023b672ecdef5b85943fad05d
Slick <147237928+Slicqq@users.noreply.github.com>
2026-02-27 18:45:39 +0530

    Add files via upload

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDBqvj-n3rXNc9X_nxcD0IUa7zZAiLW_jI",
-    authDomain: "flowcare-prototype.firebaseapp.com",
-    projectId: "flowcare-prototype",
-    storageBucket: "flowcare-prototype.firebasestorage.app",
-    messagingSenderId: "359161389605",
-    appId: "1:359161389605:web:b0a084612b538ae28255cc",
-    measurementId: "G-793Q1XF5WG"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized already
